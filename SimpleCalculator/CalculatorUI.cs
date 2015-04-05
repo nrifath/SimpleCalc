33548ae44d69b03429c09ba17ca66bcc5da46f33
tayabur-masud <msd.unknown@gmail.com>
2015-04-05 12:38:12 +0600

    Add Test.cs

---
+++ SimpleCalculator/CalculatorUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class CalculatorUI : Form
+    {
+        private double firstNo = 0;
+        private double secondNo = 0;
+        private double result = 0;
+        public CalculatorUI()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            firstNo = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNo = Convert.ToDouble(secondNumberTextBox.Text);
+            result = firstNo + secondNo;
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            firstNo = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNo = Convert.ToDouble(secondNumberTextBox.Text);
+            result = firstNo - secondNo;
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            firstNo = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNo = Convert.ToDouble(secondNumberTextBox.Text);
+            result = firstNo * secondNo;
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            firstNo = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNo = Convert.ToDouble(secondNumberTextBox.Text);
+            result = firstNo / secondNo;
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void CalculatorUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
